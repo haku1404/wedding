263399a9be5c8a58c37ff042810736752ae3b254
=Nguyen Khac Hieu <hieunk12@vingroup.net>
2026-08-13 11:43:48 +0700

    Update Google Sheets Web App URL in config.js with new version endpoint

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   SUPABASE_ANON_KEY: '',
 
   // Option 2: Google Sheets Web App URL (Active)
-  GOOGLE_SHEET_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx3wcnazUW3j1sjxIhDsk0hNcqAOAGhwZv2PRyMHELSVITaEKYpDc8SzdnM94Ope0cv/exec',
+  GOOGLE_SHEET_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxMiFH7irId69hfsFHJkQGe3jBI92F0U7cK6FBlEj1NLXwGkXQGgEf0hraNfSRYwPy3/exec',
 
   // Admin PIN code for secret delete switch
   ADMIN_PIN: '1404'
