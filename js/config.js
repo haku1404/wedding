0ced34317ef8a6f137eb6f78061c9a4174856724
=Nguyen Khac Hieu <hieunk12@vingroup.net>
2026-08-13 11:37:29 +0700

    Connect Guestbook module with Google Sheets Web App URL for live cloud storage

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
   SUPABASE_URL: '',
   SUPABASE_ANON_KEY: '',
 
-  // Option 2: Google Sheets Web App URL (Optional)
-  GOOGLE_SHEET_SCRIPT_URL: '',
+  // Option 2: Google Sheets Web App URL (Active)
+  GOOGLE_SHEET_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx3wcnazUW3j1sjxIhDsk0hNcqAOAGhwZv2PRyMHELSVITaEKYpDc8SzdnM94Ope0cv/exec',
 
   // Admin PIN code for secret delete switch
   ADMIN_PIN: '1404'
